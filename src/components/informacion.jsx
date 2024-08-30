44b18fc088f205429d790bbf6f73eec6227d578e
Matias Salazar <matias39974593@gmail.com>
2024-08-30 12:19:43 -0300

    se agrego un boton para que luego al hacer clic se abra un mensaje utilizando un estado

--- src/components/informacion.jsx
+++ src/components/informacion.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
 
-const Informacion = ({Hello}) => {
-    return (
-        <div>
-            <h1>Hello {Hello}</h1>
-        </div>
-    );
+const Informacion = ({ Hello }) => {
+    const handleClick = () => {
+        setMsj('(from changed state)');
+    }
+  return (
+    <div>
+      <h1>Hello {Hello}</h1>
+      <button className="btn btn-primary" onClick={handleClick}>
+        Click me
+      </button>
+    </div>
+  );
 };
 
 export default Informacion;
