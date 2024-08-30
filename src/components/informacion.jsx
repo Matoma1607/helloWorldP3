f3b37ab4050d63f8d8ff31c470bf1a125c6feea2
Matias Salazar <matias39974593@gmail.com>
2024-08-30 12:39:46 -0300

    se puso un estado para que al ahcer click en el boton, se vea un mensaje en pantalla

--- src/components/informacion.jsx
+++ src/components/informacion.jsx
@@ -1,12 +1,13 @@
-import React from "react";
+import { useState } from "react";
 
 const Informacion = ({ Hello }) => {
+    const [mjs, setMsj] = useState('')
     const handleClick = () => {
         setMsj('(from changed state)');
     }
   return (
     <div>
-      <h1>Hello {Hello}</h1>
+      <h1>Hello {Hello} {mjs}</h1>
       <button className="btn btn-primary" onClick={handleClick}>
         Click me
       </button>
